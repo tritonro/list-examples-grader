23bbc6f2ed84e01b7d3383a33ccaecaab8b71c43
tritonro <mdortiz@ucsd.edu>
2023-05-24 18:32:25 -0700

    meh

--- grade.sh
+++ grade.sh
@@ -5,6 +5,34 @@ git clone $1 student-submission
 echo 'Finished cloning'
 
 cp student-submission/ListExamples.java ./
+if [-f student-submission/ListExamples.java]
+then 
+    echo "ListExamples.java found"
+else 
+    echo "Error: File not found"
+fi 
+
+cp -r lib grading-area 
+cp TestListExamples.java grading-area
+cp studentsubmission/ListExamples.java grading-area 
+
+cd grading-area
+
 javac -cp $CPATH *.java
+
+if [$? -ne 0 ]
+then 
+    echo "Compiler Error"
+    exit 1
+fi 
+
 java -cp $CPATH org.junit.runner.JUnitCore TestListExamples
+
+if [$? -ne 0]
+then 
+    echo "FAILURE"
+else 
+    echo "YOU PASSED !!"
+
+fi 
 
